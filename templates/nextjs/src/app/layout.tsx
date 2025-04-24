2ab2d7d33f66b4ccc1b957561bfc8fa88d89a71e
Steve Hoang <82992510+lotusk08@users.noreply.github.com>
2025-04-24 11:25:28 +0700

    chore: change the name for the template

--- templates/nextjs/src/app/layout.tsx
+++ templates/nextjs/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css'
 
 export const metadata = {
-	title: 'tldraw Next.js app template',
-	description: 'An example of how to use tldraw in a Next.js app',
+	title: 'Page',
+	description: 'Painting tool',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
